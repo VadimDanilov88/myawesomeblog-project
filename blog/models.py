8989f8dde7254103eb7bcbdac68f67e413c743a0
Vadim <vadimdanilov88@yandex.ru>
2020-11-19 17:01:34 +0300

    Add postgres and post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db import models
 
+class Post(models.Model):
+	event_image=models.ImageField(upload_to="event_images/")
+	title=models.CharField(max_length=300)
+	data=models.DateTimeField()
+	text=models.TextField()
 # Create your models here.
--- events/models.py
+++ events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+	event_image=models.ImageField(upload_to="event_images/")
+	event_text=models.CharField(max_length=300)
+
+
+class Post(models.Model):
+	event_image=models.ImageField(upload_to="event_images/")
+	title=models.CharField(max_length=300)
+	data=models.DateTimeField()
+	text=models.TextField()
